15fa603d122a8879f669089446e259fbe104f7c2
Ostrynska <kateriinag@gmail.com>
2025-04-09 22:50:22 +0300

    fix

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer/Footer';
 import './Welcome.css';
 
 interface WelcomePageProps
@@ -7,18 +8,37 @@ interface WelcomePageProps
 
 export default function WelcomePage({ onSignIn }: WelcomePageProps) {
   return (
-    <main>
-      <section>
-        <div className="container">
-          <h1 className="welcome-title">Welcome to GitHub<br/>projects CRM!</h1>
-          <p className='welcome-text'>Sign in to manage your GitHub projects.</p>
-          <p className='welcome-text'>Once signed in, you can search for any public GitHub repository and add it to your dashboard.</p>
-          <p className='welcome-text'>Track stars, forks, issues, and release dates in one place — without switching tabs. Stay organized, focus on what matters, and simplify your workflow with real-time data from GitHub.</p>
-          <div className='welcome-btn'>
-            <button className="button-84" onClick={onSignIn}>Sign in</button>
-          </div>
-        </div>
-      </section>
-    </main>
+    <>
+        <main className='main'>
+            <section>
+            <div className="container welcome-wrapper">
+            <div className="welcome-content">
+                <div className="welcome-text-block">
+                <h1 className="welcome-title">
+                    Welcome to GitHub projects CRM!
+                </h1>
+                <p className="welcome-text">
+                    Sign in to manage your GitHub projects.
+                    Once signed in, you can search for any public GitHub repository and add it to your dashboard.
+                </p>
+                <p className="welcome-text">
+                    Track stars, forks, issues, and release dates in one place — without switching tabs. Stay organized, focus on what matters, and simplify your workflow with real-time data from GitHub.
+                </p>
+                <button className="button-84" onClick={onSignIn}>Sign in</button>
+                </div>
+                <div className="welcome-image-block">
+                <img
+                    className="welcome-image"
+                    src="/src/assets/bbeb127a-2b8f-4c3d-b626-4bc06ed2b260-Photoroom.png"
+                    alt="GitHub Projects CRM"
+                    width={360}
+                />
+                </div>
+            </div>
+            </div>
+        </section>
+        </main>
+        <Footer />
+    </>
   );
 }
